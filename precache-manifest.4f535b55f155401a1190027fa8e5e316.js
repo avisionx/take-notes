a38c7d79b2a5e2da5892bac5561b3b8f4a5aa008
avisionx <avi17223@iiitd.ac.in>
2020-09-21 17:43:50 +0000

    Deploying to gh-pages from  @ 42025c2c0f578833a616a3b3135a196b929f6bb1 🚀

---
+++ precache-manifest.4f535b55f155401a1190027fa8e5e316.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2fd490d90d5085e8241390fdea7a256",
+    "url": "/take-notes/index.html"
+  },
+  {
+    "revision": "e47170ef922aa0b41d62",
+    "url": "/take-notes/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d2beeff531a2323385d4",
+    "url": "/take-notes/static/js/2.ffc42e91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/take-notes/static/js/2.ffc42e91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e47170ef922aa0b41d62",
+    "url": "/take-notes/static/js/main.5b6d35dd.chunk.js"
+  },
+  {
+    "revision": "920dc715ae9fecb738d3",
+    "url": "/take-notes/static/js/runtime-main.b6fe9333.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/take-notes/static/media/logo.5d5d9eef.svg"
+  }
+]);
