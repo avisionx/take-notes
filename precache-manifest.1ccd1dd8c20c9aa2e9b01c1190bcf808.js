7beb9f9eb931c94fc1f1aaafb2055262979ac9ab
avisionx <avi17223@iiitd.ac.in>
2020-09-21 19:29:28 +0000

    Deploying to gh-pages from  @ e8b2247ed356906c0f9f99aaf1038edb317256d5 🚀

--- precache-manifest.1ccd1dd8c20c9aa2e9b01c1190bcf808.js
+++ precache-manifest.1ccd1dd8c20c9aa2e9b01c1190bcf808.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "186197a55be870ffc6cf652fa03a3817",
+    "revision": "abe1eb4990c2559ecf44c2b849a75226",
     "url": "/take-notes/index.html"
   },
   {
-    "revision": "dd480782b0f7effbdf69",
+    "revision": "06caab446acba265e5f7",
     "url": "/take-notes/static/css/main.0aa453fe.chunk.css"
   },
   {
-    "revision": "beffa46d86722783c4b3",
-    "url": "/take-notes/static/js/2.29bb3d4a.chunk.js"
+    "revision": "f714b3946333cfda347c",
+    "url": "/take-notes/static/js/2.ce18de12.chunk.js"
   },
   {
     "revision": "659df5b420806b55437f4f9e57f60411",
-    "url": "/take-notes/static/js/2.29bb3d4a.chunk.js.LICENSE.txt"
+    "url": "/take-notes/static/js/2.ce18de12.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "dd480782b0f7effbdf69",
-    "url": "/take-notes/static/js/main.802be6ac.chunk.js"
+    "revision": "06caab446acba265e5f7",
+    "url": "/take-notes/static/js/main.e8508a31.chunk.js"
   },
   {
     "revision": "920dc715ae9fecb738d3",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/take-notes/precache-manifest.4a9da0a257c1c471d390ccb06963e2f0.js"
+  "/take-notes/precache-manifest.1ccd1dd8c20c9aa2e9b01c1190bcf808.js"
 );
 
 self.addEventListener('message', (event) => {
