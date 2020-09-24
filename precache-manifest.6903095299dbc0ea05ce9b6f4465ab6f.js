5c4a2682a270fbe8e1b7e531c2037756ebf25c52
avisionx <avi17223@iiitd.ac.in>
2020-09-24 20:49:30 +0000

    Deploying to gh-pages from  @ a36c527722a1ddb664506c227e75f8e10f0dda56 🚀

--- precache-manifest.6903095299dbc0ea05ce9b6f4465ab6f.js
+++ precache-manifest.6903095299dbc0ea05ce9b6f4465ab6f.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fc717c3c1b9d54f32ca1f5c8c28adf97",
+    "revision": "0653032df80f60bfc2024da9a8e71d40",
     "url": "/take-notes/index.html"
   },
   {
-    "revision": "bd85dd298665712e4864",
+    "revision": "ad10931109310aa16090",
     "url": "/take-notes/static/css/main.0aa453fe.chunk.css"
   },
   {
-    "revision": "f714b3946333cfda347c",
-    "url": "/take-notes/static/js/2.ce18de12.chunk.js"
+    "revision": "9de59adf45f26a15569a",
+    "url": "/take-notes/static/js/2.9b7dd715.chunk.js"
   },
   {
     "revision": "659df5b420806b55437f4f9e57f60411",
-    "url": "/take-notes/static/js/2.ce18de12.chunk.js.LICENSE.txt"
+    "url": "/take-notes/static/js/2.9b7dd715.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "bd85dd298665712e4864",
-    "url": "/take-notes/static/js/main.5594f2d9.chunk.js"
+    "revision": "ad10931109310aa16090",
+    "url": "/take-notes/static/js/main.97230040.chunk.js"
   },
   {
     "revision": "920dc715ae9fecb738d3",
